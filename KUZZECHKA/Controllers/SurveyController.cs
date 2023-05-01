5b715dcf0bb34f87ebc1e139e1c76a6c74959f2b
Leonid Kozhanov <Terion1235@gmail.com>
2023-05-01 20:57:05 +0400

    Добавьте файлы проекта.

---
+++ KUZZECHKA/Controllers/SurveyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KUZZECHKA.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
